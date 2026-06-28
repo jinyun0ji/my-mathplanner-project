349784333ece2b0f05a38ac51ee2e6a79bca1151
수빈 <tnqls1978@naver.com>
2026-06-28 18:25:08 +0900

    수정중

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.chaesooyong.management',
   appName: '채수용 수학',
-  webDir: 'build'
+  webDir: 'build',
+
+  ios: {
+    isInspectable: true
+  }
 };
 
 export default config;
